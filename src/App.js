ad70dd093f8ec3d9fccd7de654908000e2b8a43c
Yael Brown <yaelrbrown@gmail.com>
2019-01-23 09:30:51 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        React
+
+        <Postform />
+        <hr></hr>
+        <Posts />
+
+      </div>
+    );
+  }
+}
+
+export default App;
